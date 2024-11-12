cc17d1258af938ae87533d75b2655a7a11989647
elfesolution1 <elfesolution1@gmail.com>
2024-11-12 09:18:39 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -74,7 +74,7 @@ function Page() {
             <Card className="w-[350px] mx-auto shadow-md rounded-lg overflow-hidden hover:shadow-[rgba(0,0,0,0.3)] dark:border dark:border-white dark:hover:shadow-[rgba(255,255,255,0.2)] transition-all duration-300">
               <CardHeader className="p-0">
                 <Image
-                  src={`${process.env.NEXT_PUBLIC_API_URL}/uploads/3_W7_A8840_6040c33462.webp`}
+                  src='https://sara-events-and-marketing-4fe9cca6bffd.herokuapp.com/uploads/3_W7_A8840_6040c33462.webp'
                   alt="image"
                   width={500}
                   height={210}
@@ -105,7 +105,7 @@ function Page() {
             <Card className="w-[350px] mx-auto shadow-md rounded-lg overflow-hidden hover:shadow-[rgba(0,0,0,0.3)] dark:border dark:border-white dark:hover:shadow-[rgba(255,255,255,0.2)] transition-all duration-300">
               <CardHeader className="p-0">
                 <Image
-                  src={`${process.env.NEXT_PUBLIC_API_URL}/uploads/photo_5800701578242541043_y_1_19f7c459d5.jpg`}
+                  src="https://sara-events-and-marketing-4fe9cca6bffd.herokuapp.com/uploads/photo_5800701578242541043_y_1_19f7c459d5.jpg"
                   alt="image"
                   width={500}
                   height={210}
