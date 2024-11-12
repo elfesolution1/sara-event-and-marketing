9508cb0e65719b75f786db70ab6e63c39ce5d009
elfesolution1 <elfesolution1@gmail.com>
2024-11-12 07:09:18 +0000

    typo fixed.

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"; // Adjust the path as necessary
 import { ApolloProvider } from "@apollo/client";
 // import apolloClient from "@/libs/apolloClient.js";
-import apolloClient from "@/libs/apolloclient";
+import apolloClient from "@/libs/apolloClient";
 
 // Import Inter font from Google Fonts
 const inter = Inter({
