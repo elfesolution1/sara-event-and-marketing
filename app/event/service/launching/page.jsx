b8b6420e48761066e583f9013a6ddf1893c43145
elfesolution1 <elfesolution1@gmail.com>
2024-11-30 09:14:29 +0300

    Update page.jsx

--- app/event/service/launching/page.jsx
+++ app/event/service/launching/page.jsx
@@ -114,7 +114,6 @@ function Launching() {
 
   const serviceContent = contactData?.serviceContent;
   const serviceDetail = contactData?.serviceDetail2;
-  console.log("event ", serviceDetail[0]?.children[0]?.text);
   const baseImageUrl = process.env.NEXT_PUBLIC_API_URL;
 
   const openLightbox = (src) => {
@@ -213,9 +212,7 @@ function Launching() {
                   className="relative group cursor-pointer"
                   onClick={() => openLightbox(image ?? '')}
                 >
-                  {
-              console.log('galler is is ',image)
-                  }
+                
                   <img
                     src={`${image ?? ''}`}
                     alt={image?.alt ?? 'Image'}
