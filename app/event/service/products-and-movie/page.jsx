10cfd68db98ee82b8d592b73e8e2a873e428a1b4
elfesolution1 <elfesolution1@gmail.com>
2025-02-06 03:06:27 +0300

    redup

--- app/event/service/products-and-movie/page.jsx
+++ app/event/service/products-and-movie/page.jsx
@@ -142,7 +142,7 @@ function ProductsAndMovie() {
             alt="background"
             className="w-full h-full object-cover"
           />
-          {/* <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center p-8"> */}
+          <div credlassName="absolute inset-0 bg-black bg-opacity-50 flex items-center p-8">
             <div className='flex items-center justify-center  w-full'>
               <h1 className="text-2xl font-bold mb-4 text-white leading-tight w-full text-center uppercase">
                  {contactData?.serviceTitle?.title}
@@ -151,7 +151,7 @@ function ProductsAndMovie() {
                   </span>
               </h1>
             </div>
-          {/* </div> */}
+          </div>
         </div>
       </section>
      
